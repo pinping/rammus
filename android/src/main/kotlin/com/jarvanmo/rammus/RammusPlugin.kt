1df735e72039b560490c2f85186ab5ac38c6b2b9
zhaopinping <zhaopinping@peilian.com>
2021-07-02 18:32:39 +0800

    准备修改初始化方法
    准备修改初始化方法

--- android/src/main/kotlin/com/jarvanmo/rammus/RammusPlugin.kt
+++ android/src/main/kotlin/com/jarvanmo/rammus/RammusPlugin.kt
@@ -97,6 +97,54 @@ class RammusPlugin(private val registrar: Registrar, private val methodChannel: 
                 GcmRegister.register(application.applicationContext, gcmSendId, gcmApplicationId)
             }
         }
+
+
+        /// 崩溃分析
+        @JvmStatic
+        fun initApmCrashService(appKey, appVersion, appSecret, channel,userNick, rsaPublicKey) {
+            val config = AliHaConfig()
+            config.appKey = "333467078"
+            config.appVersion = "1.0.0"
+            config.appSecret = "a8c9357857fb42a8a99518084f4ee8b1"
+            config.channel = "mqc_test"
+            config.userNick = null
+            config.application = this
+            config.context = getApplicationContext()
+            config.isAliyunos = false
+            config.rsaPublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCJBrmUo1qeVLQLGb1RqEiwgX5T90Aht8vQeLGmaPL4j3PjmXFcq4ANiU7m4ycAHs76uq" //配置项
+
+            //启动CrashReporter
+            AliHaAdapter.getInstance().addPlugin(Plugin.crashreporter) /// 崩溃分析
+            AliHaAdapter.getInstance().addPlugin(Plugin.apm) /// 性能分析
+            AliHaAdapter.getInstance().start(config)
+        }
+
+
+        /// 移动数据分析
+        @JvmStatic
+        fun initAnalysisService() {
+            /* 【注意】建议您在Application中初始化MAN，以保证正常获取MANService*/
+            // 获取MAN服务
+            val manService: MANService = MANServiceProvider.getService()
+            // 打开调试日志，线上版本建议关闭
+            // manService.getMANAnalytics().turnOnDebug();
+            // 若需要关闭 SDK 的自动异常捕获功能可进行如下操作(如需关闭crash report，建议在init方法调用前关闭crash),详见文档5.4
+            manService.getMANAnalytics().turnOffCrashReporter()
+            // 设置渠道（用以标记该app的分发渠道名称），如果不关心可以不设置即不调用该接口，渠道设置将影响控制台【渠道分析】栏目的报表展现。如果文档3.3章节更能满足您渠道配置的需求，就不要调用此方法，按照3.3进行配置即可；1.1.6版本及之后的版本，请在init方法之前调用此方法设置channel.
+            manService.getMANAnalytics().setChannel("某渠道")
+            // MAN初始化方法之一，从AndroidManifest.xml中获取appKey和appSecret初始化，若您采用上述 2.3中"统一接入的方式"，则使用当前init方法即可。
+            manService.getMANAnalytics().init(this, getApplicationContext())
+            // MAN另一初始化方法，手动指定appKey和appSecret
+            // 若您采用上述2.3中"统一接入的方式"，则无需使用当前init方法。
+            // String appKey = "******";
+            // String appSecret = "******";
+            // manService.getMANAnalytics().init(this, getApplicationContext(), appKey, appSecret);
+            // 通过此接口关闭页面自动打点功能，详见文档4.2
+            manService.getMANAnalytics().turnOffAutoPageTrack()
+            // 若AndroidManifest.xml 中的 android:versionName 不能满足需求，可在此指定
+            // 若在上述两个地方均没有设置appversion，上报的字段默认为null
+            manService.getMANAnalytics().setAppVersion("3.1.1")
+        }
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
@@ -446,48 +494,6 @@ class RammusPlugin(private val registrar: Registrar, private val methodChannel: 
 
 
 
-    /// 崩溃分析
-    private open fun initApmCrashService() {
-        val config = AliHaConfig()
-        config.appKey = "xxxxxxxx"
-        config.appVersion = "x.xx"
-        config.appSecret = "xxxxxxxxxxxx"
-        config.channel = "mqc_test"
-        config.userNick = null
-        config.application = this
-        config.context = getApplicationContext()
-        config.isAliyunos = false
-        config.rsaPublicKey = "xxxxxxx" //配置项
-        //启动CrashReporter
-        AliHaAdapter.getInstance().addPlugin(Plugin.crashreporter) /// 崩溃分析
-        AliHaAdapter.getInstance().addPlugin(Plugin.apm) /// 性能分析
-        AliHaAdapter.getInstance().start(config)
-    }
 
-
-    /// 移动数据分析
-    private open fun initAnalysisService() {
-        /* 【注意】建议您在Application中初始化MAN，以保证正常获取MANService*/
-        // 获取MAN服务
-        val manService: MANService = MANServiceProvider.getService()
-        // 打开调试日志，线上版本建议关闭
-        // manService.getMANAnalytics().turnOnDebug();
-        // 若需要关闭 SDK 的自动异常捕获功能可进行如下操作(如需关闭crash report，建议在init方法调用前关闭crash),详见文档5.4
-        manService.getMANAnalytics().turnOffCrashReporter()
-        // 设置渠道（用以标记该app的分发渠道名称），如果不关心可以不设置即不调用该接口，渠道设置将影响控制台【渠道分析】栏目的报表展现。如果文档3.3章节更能满足您渠道配置的需求，就不要调用此方法，按照3.3进行配置即可；1.1.6版本及之后的版本，请在init方法之前调用此方法设置channel.
-        manService.getMANAnalytics().setChannel("某渠道")
-        // MAN初始化方法之一，从AndroidManifest.xml中获取appKey和appSecret初始化，若您采用上述 2.3中"统一接入的方式"，则使用当前init方法即可。
-        manService.getMANAnalytics().init(this, getApplicationContext())
-        // MAN另一初始化方法，手动指定appKey和appSecret
-        // 若您采用上述2.3中"统一接入的方式"，则无需使用当前init方法。
-        // String appKey = "******";
-        // String appSecret = "******";
-        // manService.getMANAnalytics().init(this, getApplicationContext(), appKey, appSecret);
-        // 通过此接口关闭页面自动打点功能，详见文档4.2
-        manService.getMANAnalytics().turnOffAutoPageTrack()
-        // 若AndroidManifest.xml 中的 android:versionName 不能满足需求，可在此指定
-        // 若在上述两个地方均没有设置appversion，上报的字段默认为null
-        manService.getMANAnalytics().setAppVersion("3.1.1")
-    }
 
 }
