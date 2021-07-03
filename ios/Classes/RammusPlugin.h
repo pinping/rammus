9a33e2d3b0d67e52fe4b777380bdb0df04b7ab0b
Pinping <377795496@163.com>
2021-07-04 01:27:52 +0800

    修改依赖.解决错误.

--- ios/Classes/RammusPlugin.h
+++ ios/Classes/RammusPlugin.h
@@ -2,6 +2,9 @@
 #import <UserNotifications/UserNotifications.h>
 #import <CloudPushSDK/CloudPushSDK.h>
 #import <AlicloudMobileAnalitics/ALBBMAN.h>
+#import <AlicloudCrash/AlicloudCrashProvider.h>
+#import <AlicloudAPM/AlicloudAPMProvider.h>
+#import <AlicloudHAUtil/AlicloudHAProvider.h>
 
 @interface RammusPlugin : NSObject<FlutterPlugin,UNUserNotificationCenterDelegate>
 @end
