ef94c9724b69c9c678d1752ab340c350c975c849
Pinping <377795496@163.com>
2021-07-03 23:41:32 +0800

    添加EMAS调用.

--- android/src/main/kotlin/com/jarvanmo/rammus/RammusPlugin.kt
+++ android/src/main/kotlin/com/jarvanmo/rammus/RammusPlugin.kt
@@ -121,7 +121,7 @@ class RammusPlugin(private val registrar: Registrar, private val methodChannel: 
             config.channel = aliChannel
             config.userNick = aliUserNick
             config.application = this
-            config.context = getApplicationContext()
+            config.context = application.applicationContext
             config.isAliyunos = false
             config.rsaPublicKey = aliRsaPublicKey //配置项
 
@@ -134,7 +134,7 @@ class RammusPlugin(private val registrar: Registrar, private val methodChannel: 
 
         /// 移动数据分析
         @JvmStatic
-        fun initAnalysisService() {
+        fun initAnalysisService(application: Application) {
             /* 【注意】建议您在Application中初始化MAN，以保证正常获取MANService*/
             // 获取MAN服务
             val manService: MANService = MANServiceProvider.getService()
@@ -143,9 +143,9 @@ class RammusPlugin(private val registrar: Registrar, private val methodChannel: 
             // 若需要关闭 SDK 的自动异常捕获功能可进行如下操作(如需关闭crash report，建议在init方法调用前关闭crash),详见文档5.4
             manService.getMANAnalytics().turnOffCrashReporter()
             // 设置渠道（用以标记该app的分发渠道名称），如果不关心可以不设置即不调用该接口，渠道设置将影响控制台【渠道分析】栏目的报表展现。如果文档3.3章节更能满足您渠道配置的需求，就不要调用此方法，按照3.3进行配置即可；1.1.6版本及之后的版本，请在init方法之前调用此方法设置channel.
-            manService.getMANAnalytics().setChannel("某渠道")
+            manService.getMANAnalytics().setChannel("官方道")
             // MAN初始化方法之一，从AndroidManifest.xml中获取appKey和appSecret初始化，若您采用上述 2.3中"统一接入的方式"，则使用当前init方法即可。
-            manService.getMANAnalytics().init(this, getApplicationContext())
+            manService.getMANAnalytics().init(this, application.applicationContext)
             // MAN另一初始化方法，手动指定appKey和appSecret
             // 若您采用上述2.3中"统一接入的方式"，则无需使用当前init方法。
             // String appKey = "******";
@@ -155,7 +155,7 @@ class RammusPlugin(private val registrar: Registrar, private val methodChannel: 
             manService.getMANAnalytics().turnOffAutoPageTrack()
             // 若AndroidManifest.xml 中的 android:versionName 不能满足需求，可在此指定
             // 若在上述两个地方均没有设置appversion，上报的字段默认为null
-            manService.getMANAnalytics().setAppVersion("3.1.1")
+            manService.getMANAnalytics().setAppVersion("1.0.0")
         }
     }
 
